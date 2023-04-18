9b5f0887bc3cf4253bcc77cc3f4f94e6eff8fae0
Iván <mrmaya.70797@gmail.com>
2023-04-18 03:23:58 -0600

    Nueva funcion Openpage despues de animacion

--- eventos.js
+++ eventos.js
@@ -1,12 +1,19 @@
 function showName() {
     var name = document.getElementById("name-input").value;
-    document.querySelector(".welcome-text").innerHTML = name;
-    document.querySelector(".flip-card-inner").style.transform = "rotateY(180deg)";
+  document.querySelector(".welcome-text").innerHTML = name;
+  document.querySelector(".flip-card-inner").style.transform = "rotateY(180deg)";
+  if (name === "") {
+    document.querySelector(".welcome-text").innerHTML = "Anonimo";
   }
-  
-    
-    function checkEnter(event) {
-      if (event.keyCode === 13) {
-        showName();
-      }
-    }
+  setTimeout(openPage, 2000);
+}
+
+function openPage() {
+  window.location.href = "Aluraencriptor.html";
+}
+
+function checkEnter(event) {
+  if (event.keyCode === 13) {
+    showName();
+  }
+}
