bea4eb958f14eaa7ab52ec15414d798be2bdf40a
Iván <mrmaya.70797@gmail.com>
2023-04-19 03:51:29 -0600

    JS: Actualización para almacenar el nombre ingresado por el usuario en el almacenamiento local, mostrarlo en la página de bienvenida, agregar funciones de encriptación y desencriptación, y verificar si la página actual es la de encriptación. También se mejoró la función showName y se eliminó la redirección automática.

--- eventos.js
+++ eventos.js
@@ -1,13 +1,19 @@
 function showName() {
-    var name = document.getElementById("name-input").value;
+  var nameInputValue = document.getElementById("name-input").value;
+  if (nameInputValue.trim() !== "") {
+    localStorage.setItem("name", nameInputValue.trim());
+  } if (nameInputValue.trim() === "") {
+    localStorage.setItem("name", "Anonimo");
+  }
+  var name = localStorage.getItem("name");
   document.querySelector(".welcome-text").innerHTML = name;
   document.querySelector(".flip-card-inner").style.transform = "rotateY(180deg)";
-  if (name === "") {
-    document.querySelector(".welcome-text").innerHTML = "Anonimo";
-  }
   setTimeout(openPage, 2000);
 }
-
+function name() {
+  var name = localStorage.getItem("name");
+  document.querySelector(".Inputedname").innerHTML = "Bienvenido " + name + " , esta todo listo para comenzar a encriptar...";
+}
 function openPage() {
   window.location.href = "Aluraencriptor.html";
 }
@@ -16,4 +22,30 @@ function checkEnter(event) {
   if (event.keyCode === 13) {
     showName();
   }
+}
+
+if (window.location.href.endsWith("Aluraencriptor.html")) {
+  window.onload = function() {
+    name();
+  };
+}
+
+function encriptar() {
+  let texto = document.getElementById("text-to-encode").value;
+  let textoEncriptado = texto.replace(/e/g, "enter");
+  textoEncriptado = textoEncriptado.replace(/i/g, "imes");
+  textoEncriptado = textoEncriptado.replace(/a/g, "ai");
+  textoEncriptado = textoEncriptado.replace(/o/g, "ober");
+  textoEncriptado = textoEncriptado.replace(/u/g, "ufat");
+  document.getElementById("resultado").innerHTML = textoEncriptado;
+}
+
+function desencriptar() {
+  let texto = document.getElementById("text-to-encode").value;
+  let textodes = texto.replace(/enter/g, "e");
+  textodes = textodes.replace(/imes/g, "i");
+  textodes = textodes.replace(/ai/g, "a");
+  textodes = textodes.replace(/ober/g, "o");
+  textodes = textodes.replace(/ufat/g, "u");
+  document.getElementById("resultado").innerHTML = textodes;
 }
